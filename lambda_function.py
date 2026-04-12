e375082dbd6cc126fb2751292b77f8ddd7df9e5e
wilfriedbako <bakowilfriedmarcel@gmail.com>
2026-04-11 21:16:55 -0400

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,12 +1,28 @@
+import boto3
 import hashlib
 
+s3 = boto3.client('s3')
+
 def lambda_handler(event, context):
-    file_content = "sample data"
 
-    file_hash = hashlib.sha256(file_content.encode()).hexdigest()
+    # Get bucket + file from S3 event
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = event['Records'][0]['s3']['object']['key']
 
-    print("File received")
-    print("SHA256 Hash:", file_hash)
+    print(f"Bucket: {bucket}")
+    print(f"File: {key}")
+
+    # Get file from S3
+    obj = s3.get_object(Bucket=bucket, Key=key)
+
+    # Hash file
+    sha256 = hashlib.sha256()
+    for chunk in obj['Body'].iter_chunks():
+        sha256.update(chunk)
+
+    file_hash = sha256.hexdigest()
+
+    print(f"SHA256: {file_hash}")
 
     return {
         "statusCode": 200,
