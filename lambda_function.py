69a8371feba12415cc13e45cf84c188a1cee590d
wilfriedbako <bakowilfriedmarcel@gmail.com>
2026-04-12 21:02:05 -0400

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -4,22 +4,22 @@ from datetime import datetime
 
 s3 = boto3.client('s3')
 dynamodb = boto3.resource('dynamodb')
+sns = boto3.client('sns')
 
 table = dynamodb.Table('aegis-audit-table')
 
+SNS_TOPIC_ARN = 'arn:aws:sns:us-east-1:490848272326:Project-aegis-alerts'
+
 def lambda_handler(event, context):
 
-    # Get bucket and file
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = event['Records'][0]['s3']['object']['key']
 
     print(f"Bucket: {bucket}")
     print(f"File: {key}")
 
-    # Get file
     obj = s3.get_object(Bucket=bucket, Key=key)
 
-    # Hash file
     sha256 = hashlib.sha256()
     for chunk in obj['Body'].iter_chunks():
         sha256.update(chunk)
@@ -29,7 +29,27 @@ def lambda_handler(event, context):
 
     print(f"SHA256: {file_hash}")
 
-    # Save to DynamoDB
+    # 🔎 Check if file already exists
+    response = table.query(
+        KeyConditionExpression=boto3.dynamodb.conditions.Key('file_name').eq(key)
+    )
+
+    items = response.get('Items', [])
+
+    # 🚨 Compare hashes
+    if items:
+        last_hash = items[-1]['hash']
+
+        if last_hash != file_hash:
+            print("⚠️ Possible tampering detected!")
+
+            sns.publish(
+                TopicArn=SNS_TOPIC_ARN,
+                Subject="🚨 File Integrity Alert",
+                Message=f"File {key} has been modified!\nOld hash: {last_hash}\nNew hash: {file_hash}"
+            )
+
+    # 💾 Save new record
     table.put_item(
         Item={
             'file_name': key,
