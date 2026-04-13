2044d0ed9e360744dbc1738bd8bc0d6120de6ed9
wilfriedbako <bakowilfriedmarcel@gmail.com>
2026-04-12 20:29:24 -0400

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,18 +1,22 @@
 import boto3
 import hashlib
+from datetime import datetime
 
 s3 = boto3.client('s3')
+dynamodb = boto3.resource('dynamodb')
+
+table = dynamodb.Table('aegis-audit-table')
 
 def lambda_handler(event, context):
 
-    # Get bucket + file from S3 event
+    # Get bucket and file
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = event['Records'][0]['s3']['object']['key']
 
     print(f"Bucket: {bucket}")
     print(f"File: {key}")
 
-    # Get file from S3
+    # Get file
     obj = s3.get_object(Bucket=bucket, Key=key)
 
     # Hash file
@@ -21,8 +25,21 @@ def lambda_handler(event, context):
         sha256.update(chunk)
 
     file_hash = sha256.hexdigest()
+    timestamp = datetime.utcnow().isoformat()
 
     print(f"SHA256: {file_hash}")
+
+    # Save to DynamoDB
+    table.put_item(
+        Item={
+            'file_name': key,
+            'timestamp': timestamp,
+            'hash': file_hash,
+            'bucket': bucket
+        }
+    )
+
+    print("Saved to DynamoDB")
 
     return {
         "statusCode": 200,
